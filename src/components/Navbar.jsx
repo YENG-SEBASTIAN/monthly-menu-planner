ab6038a832a52828e3d49d66127f3bedd8477d06
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-07-30 10:38:55 +0200

    Planner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,20 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-primary">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Abena Monthly Menu Planner</Link>
+        <Link className="navbar-brand text-white" to="/">Abena Monthly Menu Planner</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon text-white"></span>
         </button>
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link fw-bold" to="/">Home</Link>
+              <Link className="nav-link fw-bold text-white" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link fw-bold" to="/recipes">Recipes</Link>
+              <Link className="nav-link fw-bold text-white" to="/recipes">Recipes</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link fw-bold" to="/menus">Menus</Link>
+              <Link className="nav-link fw-bold text-white" to="/menus">Menus</Link>
             </li>
           </ul>
         </div>
